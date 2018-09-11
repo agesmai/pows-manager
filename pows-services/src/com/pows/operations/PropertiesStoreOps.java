5133d9fd1a5144cc1d646f886231965ebc4ff89e
anhpv1602 <anhpv1602@gmail.com>
2018-09-11 16:54:03 +0700

    rebuild pows-admin

--- pows-services/src/com/pows/operations/PropertiesStoreOps.java
+++ pows-services/src/com/pows/operations/PropertiesStoreOps.java
@@ -1,4 +1,18 @@
 package com.pows.operations;
 
+import com.pows.entity.PowsResponse;
+
 public interface PropertiesStoreOps {
+
+    PowsResponse getAllBySystemCode();
+
+    PowsResponse searchByCode();
+
+    PowsResponse searchByName();
+
+    PowsResponse add();
+
+    PowsResponse modify();
+
+    PowsResponse remove();
 }
